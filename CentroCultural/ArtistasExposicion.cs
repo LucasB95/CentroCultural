3592a354ec72cd68094e4b8071eb040d8dc93c70
Lucas Basualdo <basualdo1995@gmail.com>
2021-04-24 13:10:37 -0300

    subida de archivos

---
+++ CentroCultural/ArtistasExposicion.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CentroCultural
+{
+    class ArtistasExposicion
+    {
+        public Artista art;
+        //public int[] ArtistasExp;
+        List<Artista> ArtistasExp = new List<Artista> { };
+
+
+        public ArtistasExposicion(){
+           
+        }
+       public void insertarArtista(Artista a){
+
+            ArtistasExp.Add(a);
+    
+        }
+
+
+        public int cantidadArtistas()
+        {
+            int contArtistas = 0;
+            foreach (Artista art in ArtistasExp)
+            {
+                contArtistas++;
+            }
+            return contArtistas;
+        }
+
+        
+        public Boolean estaArtista(ObraDeArte o)
+        {
+            Boolean estaArt = false;
+            foreach(Artista art in ArtistasExp)
+            {
+                if (o.Nombre().Equals(art.Nombre()))
+                {
+                    estaArt = true;
+                }
+            }
+            return estaArt;
+        }
+
+        public Boolean estaLlena()
+        {
+            Boolean estaLleno = true;
+            foreach (Artista art in ArtistasExp)
+            {
+                if (art == null)
+                {
+                    estaLleno = false;
+                }
+            }
+            return estaLleno;
+        }
+
+
+        public Boolean hayArtista()
+        {
+            Boolean hayArt = true;  
+                if (ArtistasExp == null || ArtistasExp.Count == 0)
+                {
+                    hayArt = false;
+                }
+   
+            return hayArt;
+        }
+
+        
+        public Artista recuperaArtista(String nom){
+
+            Artista recuart = null;
+            foreach (Artista art in ArtistasExp)
+            {
+                if (nom.Equals(art.Nombre()))
+                {
+                    recuart = art;
+                }
+            }
+            return recuart;
+        }
+
+        //Este metodo es el que cumple la funcion del metodo artistasNac pero con otra funcionalidad diferente al de la linea 103
+        public Artista recuperaNacionalidad(String Nac)
+        {
+
+            Artista recuart = null;
+            foreach (Artista art in ArtistasExp)
+            {
+                if (Nac.Equals(art.Nacionalidad()))
+                {
+                    recuart = art;
+                }
+            }
+            return recuart;
+        }
+
+
+        public List<Artista> artistasNac(String Nac)
+        {
+            List<Artista> artnac = new List<Artista> { };
+            foreach (Artista art in ArtistasExp)
+            {
+                if (art.Nacionalidad().Equals(Nac))
+                {
+                    artnac.Add(art);
+                }
+            }
+            return artnac;
+        }
+
+        
+
+
+    }
+}
---
+++ CentroCultural/CentroCultural.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CentroCultural
+{
+    class CentroCultural
+    {
+        private String nombre;
+        public ObrasExposicion obras;
+        private ArtistasExposicion artistas;
+
+        public CentroCultural(String nombre,ObrasExposicion obras,ArtistasExposicion art)
+        {
+            this.nombre = nombre;
+            this.obras = obras;
+            this.artistas = art;
+
+        }
+
+
+        public List<String> nombresObrasNacionalidad(String nac)
+        {
+            List<String> nombres = new List<String> { };
+       
+            List<Artista> listaArtistas = artistas.artistasNac(nac);
+            foreach (Artista art in listaArtistas)
+            {
+                nombres.Add(art.Nombre());
+            }
+            return nombres;
+        }
+     
+        public List<String> nombresCuadrosGaleria(String Gal)
+        {
+            List<String> nombres = new List<String> { };
+
+            List<ObraDeArte> listacuadros = obras.todosLosCuadrosPrestados();
+            foreach (ObraDeArte obr in listacuadros)
+            {
+                nombres.Add(obr.Nombre());
+            }
+            return nombres;
+        }
+
+        public void Nombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+        public String Nombre()
+        {
+            return nombre;
+        }
+        public void Obras(ObrasExposicion obra)
+        {
+            this.obras = obra;
+        }
+        public ObrasExposicion Obras()
+        {
+            return obras;
+        }
+        public void Artistas(ArtistasExposicion art)
+        {
+            this.artistas = art;
+        }
+        public ArtistasExposicion Artistas()
+        {
+            return artistas;
+        }
+
+      
+        public String nombreCuadrosGaleria(String Gal)
+        {
+            return Gal;
+        }
+
+    }
+}
